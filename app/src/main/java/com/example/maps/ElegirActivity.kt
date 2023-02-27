0f172159950745bbae9fbf4a3c4fc6d4a20f5df7
Daniel <dmarrom577@g.educaand.es>
2023-02-27 14:17:25 +0100

    version casa

--- app/src/main/java/com/example/maps/ElegirActivity.kt
+++ app/src/main/java/com/example/maps/ElegirActivity.kt
@@ -3,12 +3,14 @@ package com.example.maps
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.model.LatLng
 
 class ElegirActivity:AppCompatActivity() {
     private lateinit var aniadir:Button
     private lateinit var buscar:Button
+    private lateinit var aeropuertos:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,22 +18,29 @@ class ElegirActivity:AppCompatActivity() {
 
         aniadir = findViewById(R.id.button)
         buscar = findViewById(R.id.button2)
+        aeropuertos = findViewById(R.id.button3)
 
         val bundle = intent
         val coordinates = bundle.getParcelableExtra<LatLng>("UBI")
         val end = bundle?.getStringExtra("END")
 
         buscar.setOnClickListener {
-            val i = Intent(this, MapaFinal::class.java).apply {
-                putExtra("UBIFINAL", coordinates)
-                putExtra("ENDFINAL", end)
-
+            if (coordinates==null || end==null) {
+                Toast.makeText(this, "Debe añadir una ubicacion primero", Toast.LENGTH_SHORT).show()
+            } else {
+                val i = Intent(this, MapaFinal::class.java).apply {
+                    putExtra("UBIFINAL", coordinates)
+                    putExtra("ENDFINAL", end)
+                }
+                startActivity(i)
             }
-            startActivity(i)
         }
-
         aniadir.setOnClickListener{
             startActivity(Intent(this, MainActivity::class.java))
+        }
+
+        aeropuertos.setOnClickListener {
+            startActivity(Intent(this, MapaAeropuertos::class.java))
         }
     }
 }
--- app/src/main/java/com/example/maps/MapaAeropuertos.kt
+++ app/src/main/java/com/example/maps/MapaAeropuertos.kt
@@ -1,12 +1,16 @@
 package com.example.maps
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
@@ -28,18 +32,26 @@ import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class MapaFinal : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener {
+class MapaAeropuertos : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener {
     private lateinit var map:GoogleMap
     private lateinit var btnCalculate: Button
+    private lateinit var btnVamonos:Button
 
     private var start:String=""
     private var end:String=""
 
     var poly:Polyline? = null
+
+    //Aeropuertos
+    private lateinit var coordinatesGranada:LatLng
+    private lateinit var coordinatesAlmeria:LatLng
+    private lateinit var coordinatesMalaga:LatLng
+    private lateinit var coordinatesMadrid:LatLng
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.ruta)
-        btnCalculate = findViewById(R.id.btnCalculateRoute)
+        setContentView(R.layout.aeropuertos)
+        btnCalculate = findViewById(R.id.btnIrA)
 
         btnCalculate.setOnClickListener {
             val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
@@ -49,7 +61,7 @@ class MapaFinal : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMyLocatio
             }
             poly?.remove()
             start=""
-            //end=""
+            end=""
             poly = null
             if(::map.isInitialized) {
                 val fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
@@ -71,14 +83,60 @@ class MapaFinal : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMyLocatio
                     // for ActivityCompat#requestPermissions for more details.
                     return@setOnClickListener
                 }
+
                 fusedLocationClient.lastLocation
                     .addOnSuccessListener { location: Location? ->
                         // Se ha obtenido la ubicación actual
                         if (location != null) {
                             val latitude = location?.latitude
                             val longitude = location?.longitude
+                            val coordinates = LatLng(latitude!!, longitude!!)
                             start="${longitude},${latitude}"
-                            createRoute()
+                            var options = listOf("Aeropuerto Madrid", "Aeropuerto Granada", "Aeropuerto Málaga", "Aeropuerto Almería")
+                            val builder = AlertDialog.Builder(this)
+                            builder.setTitle("Elige el aeropuerto al que deseas llegar")
+                            builder.setItems(options.toTypedArray()){ _, item ->
+                                when {
+                                    options[item] == "Aeropuerto Madrid" -> {
+                                        end = "${-3.593806}, ${40.492153}"
+                                        createRoute()
+                                        map.animateCamera(
+                                            CameraUpdateFactory.newLatLngZoom(coordinates, 6f),
+                                            2000,
+                                            null
+                                        )
+                                    }
+                                    options[item] == "Aeropuerto Granada" -> {
+                                        end = "${-3.778166}, ${37.187297}"
+                                        createRoute()
+                                        map.animateCamera(
+                                            CameraUpdateFactory.newLatLngZoom(coordinates, 8f),
+                                            2000,
+                                            null
+                                        )
+                                    }
+                                    options[item] == "Aeropuerto Málaga" -> {
+                                        end = "${-4.491321}, ${36.676559}"
+                                        createRoute()
+                                        map.animateCamera(
+                                            CameraUpdateFactory.newLatLngZoom(coordinates, 7f),
+                                            2000,
+                                            null
+                                        )
+                                    }
+                                    options[item] == "Aeropuerto Almería" -> {
+                                        end = "${-2.370763}, ${36.844627}"
+                                        createRoute()
+                                        map.animateCamera(
+                                            CameraUpdateFactory.newLatLngZoom(coordinates, 9f),
+                                            2000,
+                                            null
+                                        )
+                                    }
+                                }
+                            }
+                            builder.show()
+
                         }
                     }
             }
@@ -128,26 +186,68 @@ class MapaFinal : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMyLocatio
 
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
-        createMarker()
         map.setOnMyLocationButtonClickListener(this)
         enableLocation()
-        //metodo()
+        //Ponemos valores a los aeropuertos
+        coordinatesGranada = LatLng(37.187297, -3.778166)
+        coordinatesAlmeria = LatLng(36.844627, -2.370763)
+        coordinatesMalaga = LatLng(36.676559, -4.491321)
+        coordinatesMadrid = LatLng(40.492153, -3.593806)
+        createMarker("${-3.778166}, ${37.187297}", coordinatesGranada, "Aeropuerto Granada")
+        createMarker("${-2.370763}, ${36.844627}", coordinatesAlmeria, "Aeropuerto Almería")
+        createMarker("${-4.491321}, ${36.676559}", coordinatesMalaga, "Aeropuerto Málaga")
+        createMarker("${-3.593806}, ${40.492153}", coordinatesMadrid, "Aeropuerto Madrid")
+        //Este codigo es para cuando al estar el mapa
+        //listo hace una animación para posicionarlo
+        //justo donde esté la ubicación el tiempo real
+        //del gps
+        val fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+                // Se ha obtenido la ubicación actual
+                if (location != null) {
+                    val latitude = location?.latitude
+                    val longitude = location?.longitude
+                    val coordinates = LatLng(latitude!!, longitude!!)
+                    map.animateCamera(
+                        CameraUpdateFactory.newLatLngZoom(coordinates, 15f),
+                        4000,
+                        null
+                    )
+
+                }
+            }
     }
 
-    private fun createMarker() {
-        val bundle = intent
-        val coordinates = bundle.getParcelableExtra<LatLng>("UBIFINAL")
-        val destino = bundle?.getStringExtra("ENDFINAL")
-        end = destino.toString()
-        println(coordinates)
+    private fun createMarker(ubicacion:String, marcador:LatLng, aeropuerto:String) {
+        val coordinates = marcador
+        end = ubicacion.toString()
         //val coordinates = LatLng(36.781853, -2.815791)
-        val marker:MarkerOptions = MarkerOptions().position(coordinates!!).title("Ubicación")
+        val marker:MarkerOptions = MarkerOptions().position(coordinates!!).title(aeropuerto)
         map.addMarker(marker)
-        map.animateCamera(
+        /*map.animateCamera(
             CameraUpdateFactory.newLatLngZoom(coordinates, 15f),
             4000,
             null
-        )
+        )*/
     }
 
     private fun isLocationPermissionGranted() = ContextCompat.checkSelfPermission(
